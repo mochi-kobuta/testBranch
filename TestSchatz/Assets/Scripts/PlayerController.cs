cf16c2bb5b6602e747c2226251cd6cf54f61bc2e
Yuuki Kobayashi <ecruze_birth@yahoo.co.jp>
2018-10-24 01:31:19 +0900

    walkアニメーション追加

--- TestSchatz/Assets/Scripts/PlayerController.cs
+++ TestSchatz/Assets/Scripts/PlayerController.cs
@@ -18,24 +18,28 @@ public class PlayerController : MonoBehaviour {
         {
             transform.Translate(0, -ConstantList.PLAYER_MOVE_SPEED, 0);
             this.AniCtrl.SetBool("DownWalk",true);
+            this.allIdolClear();
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(0, ConstantList.PLAYER_MOVE_SPEED, 0);
             this.AniCtrl.SetBool("UpWalk", true);
+            this.allIdolClear();
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(-ConstantList.PLAYER_MOVE_SPEED, 0, 0);
             this.AniCtrl.SetBool("LeftWalk", true);
+            this.allIdolClear();
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(ConstantList.PLAYER_MOVE_SPEED, 0, 0);
             this.AniCtrl.SetBool("RightWalk", true);
+            this.allIdolClear();
         }
 
         
@@ -43,6 +47,7 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             this.AniCtrl.SetBool("DownWalk", false);
+            this.AniCtrl.SetBool("DownIdol", true);
         }
 
         if (Input.GetKeyUp(KeyCode.LeftArrow))
@@ -54,14 +59,23 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
             this.AniCtrl.SetBool("UpWalk", false);
+            this.AniCtrl.SetBool("UpIdol", true);
         }
 
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             this.AniCtrl.SetBool("RightWalk", false);
+            this.AniCtrl.SetBool("RightIdol", true);
         }
    
-        
+    }
+
+    void allIdolClear()
+    {
+        this.AniCtrl.SetBool("DownIdol", false);
+        this.AniCtrl.SetBool("UpIdol", false);
+        this.AniCtrl.SetBool("LeftIdol", false);
+        this.AniCtrl.SetBool("RightIdol", false);
     }
 
 
