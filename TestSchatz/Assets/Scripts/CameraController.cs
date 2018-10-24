e54604887052f474f28d2669f224ffe23e6b08cf
yuuki kobayashi <y_kobayashi@serori.co.jp>
2018-10-24 17:55:16 +0900

    街オブジェクト & カメラ追跡

---
+++ TestSchatz/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+  public GameObject followTargetObject;
+	
+  void Start () {
+		
+	}
+	
+	void Update () {
+    gameObject.transform.position = new Vector3(followTargetObject.transform.position.x, 0, -10);
+	}
+}
