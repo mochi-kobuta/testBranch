9f9a9d61f1d2155e30e4a2ba11b8b59e0ac5331b
yuuki kobayashi <y_kobayashi@serori.co.jp>
2018-10-25 19:04:16 +0900

    画面端表示処理追加

--- TestSchatz/Assets/Scripts/CameraController.cs
+++ TestSchatz/Assets/Scripts/CameraController.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using GameConstant;
 
 public class CameraController : MonoBehaviour {
 
   public GameObject followTargetObject;
-	
+
+  //入り口の位置情報
+  public GameObject startPoint = null;
+
+  //出口の位置情報
+  public GameObject endPoint = null;
+
   void Start () {
-		
+
+    
 	}
 	
 	void Update () {
-    gameObject.transform.position = new Vector3(followTargetObject.transform.position.x, 0, -10);
-	}
+
+    //入り口と出口の情報があった場合カメラの追随制限をかける
+    if(endPoint != null && endPoint != null)
+    { 
+      var sPos = followTargetObject.transform.position.x - startPoint.transform.position.x;
+      var ePos = endPoint.transform.position.x - followTargetObject.transform.position.x;
+      if (sPos <= ConstantList.MAIN_CAMERA_WIDTH_SIZE)
+      {
+        gameObject.transform.position = new Vector3(startPoint.transform.position.x + ConstantList.MAIN_CAMERA_WIDTH_SIZE, followTargetObject.transform.position.y + 1.5f, -10);
+      } else if (ePos <= 4) {
+        gameObject.transform.position = new Vector3(endPoint.transform.position.x - ConstantList.MAIN_CAMERA_WIDTH_SIZE, followTargetObject.transform.position.y + 1.5f, -10);
+      } else {
+        gameObject.transform.position = new Vector3(followTargetObject.transform.position.x, followTargetObject.transform.position.y + 1.5f, -10);
+      }
+    } else {
+      gameObject.transform.position = new Vector3(followTargetObject.transform.position.x, followTargetObject.transform.position.y + 1.5f, -10);
+    }
+
+
+
+
+  }
 }
--- TestSchatz/Assets/Scripts/GameConstant.cs
+++ TestSchatz/Assets/Scripts/GameConstant.cs
@@ -6,5 +6,9 @@ namespace GameConstant {
     public class ConstantList : MonoBehaviour {
 
         public const float PLAYER_MOVE_SPEED = 0.025f;
-    }
+
+        //カメラオブジェクトのSizeによって値調整
+        public const float MAIN_CAMERA_WIDTH_SIZE = 4.0f;
+
+  }
 }
