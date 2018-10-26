c56ef8f76f8b5db761c6ef52d615ae4ff25a834e
yuuki kobayashi <y_kobayashi@serori.co.jp>
2018-10-26 16:29:31 +0900

    Y軸方向プレイヤースクロール　カメラ追随処理

--- TestSchatz/Assets/Scripts/CameraController.cs
+++ TestSchatz/Assets/Scripts/CameraController.cs
@@ -6,40 +6,96 @@ using GameConstant;
 
 public class CameraController : MonoBehaviour {
 
+  //視点を追うオブジェクト
   public GameObject followTargetObject;
 
-  //入り口の位置情報
-  public GameObject startPoint = null;
+  //視点オブジェクトの初期位置調整関数
+  public float followTargetObjectPositionX;
+  public float followTargetObjectPositionY;
 
-  //出口の位置情報
-  public GameObject endPoint = null;
+  //入り口と出口の情報（X軸方向）
+  public GameObject startPointX = null;
+  public GameObject endPointX = null;
+
+  //入り口と出口の情報（Y軸方向）
+  public GameObject startPointY = null;
+  public GameObject endPointY = null;
+
+  //メンバ変数
+  private bool pointCheckFlag = false;
+  private Vector3 commitCameraPosition = new Vector3();
 
   void Start () {
-
-    
-	}
+      pointCheckFlag = pointCheck();
+  }
 	
 	void Update () {
 
-    //入り口と出口の情報があった場合カメラの追随制限をかける
-    if(endPoint != null && endPoint != null)
+    //X方向と方向の入り口と出口の情報があった場合カメラの追随制限をかける
+    //Debug.Log(pointCheckFlag);
+    if (pointCheckFlag)
     { 
-      var sPos = followTargetObject.transform.position.x - startPoint.transform.position.x;
-      var ePos = endPoint.transform.position.x - followTargetObject.transform.position.x;
-      if (sPos <= ConstantList.MAIN_CAMERA_WIDTH_SIZE)
-      {
-        gameObject.transform.position = new Vector3(startPoint.transform.position.x + ConstantList.MAIN_CAMERA_WIDTH_SIZE, followTargetObject.transform.position.y + 1.5f, -10);
-      } else if (ePos <= 4) {
-        gameObject.transform.position = new Vector3(endPoint.transform.position.x - ConstantList.MAIN_CAMERA_WIDTH_SIZE, followTargetObject.transform.position.y + 1.5f, -10);
-      } else {
-        gameObject.transform.position = new Vector3(followTargetObject.transform.position.x, followTargetObject.transform.position.y + 1.5f, -10);
-      }
+        var sPosX = followTargetObject.transform.position.x - startPointX.transform.position.x;
+        var ePosX = endPointX.transform.position.x - followTargetObject.transform.position.x;
+        var sPosY = followTargetObject.transform.position.y - startPointY.transform.position.y;
+        var ePosY = endPointY.transform.position.y - followTargetObject.transform.position.y;
+
+        //カメラ追随位置初期化
+        if(followTargetObjectPositionX > 0)
+        {
+            commitCameraPosition.x = followTargetObject.transform.position.x + (float)followTargetObjectPositionX;
+        } else {
+            commitCameraPosition.x = followTargetObject.transform.position.x;
+        }
+
+        if (followTargetObjectPositionY > 0)
+        {
+            commitCameraPosition.y = followTargetObject.transform.position.y + (float)followTargetObjectPositionY;
+        } else {
+            commitCameraPosition.y = followTargetObject.transform.position.y;
+        }
+
+
+        if (sPosX <= ConstantList.MAIN_CAMERA_WIDTH_SIZE)
+        {
+            commitCameraPosition.x = startPointX.transform.position.x + ConstantList.MAIN_CAMERA_WIDTH_SIZE;
+        }
+
+        if (ePosX <= ConstantList.MAIN_CAMERA_WIDTH_SIZE) {
+            commitCameraPosition.x = endPointX.transform.position.x - ConstantList.MAIN_CAMERA_WIDTH_SIZE;
+        }
+
+        if (sPosY <= ConstantList.MAIN_CAMERA_HEIGHT_SIZE) {
+            commitCameraPosition.y = startPointY.transform.position.y + ConstantList.MAIN_CAMERA_HEIGHT_SIZE;
+        }
+
+        if (ePosY <= ConstantList.MAIN_CAMERA_HEIGHT_SIZE) {
+            commitCameraPosition.y = endPointY.transform.position.y - ConstantList.MAIN_CAMERA_HEIGHT_SIZE;
+        }
+
+        gameObject.transform.position = new Vector3(commitCameraPosition.x, commitCameraPosition.y, ConstantList.INIT_CAMERA_POSITION_Z);
+
     } else {
-      gameObject.transform.position = new Vector3(followTargetObject.transform.position.x, followTargetObject.transform.position.y + 1.5f, -10);
+        gameObject.transform.position = new Vector3(followTargetObject.transform.position.x, followTargetObject.transform.position.y + ConstantList.CITY1_PLAYER_INIT_POSITION_Y, ConstantList.INIT_CAMERA_POSITION_Z);
     }
 
+  }
 
+  //エントリーポイントとアウトポイントがセットされているか
+  bool pointCheck()
+  {
+      if (startPointX == null)
+          return false;
 
+      if (endPointX == null)
+          return false;
 
+      if (startPointY == null)
+          return false;
+
+      if (endPointY == null)
+          return false;
+
+      return true;
   }
 }
--- TestSchatz/Assets/Scripts/GameConstant.cs
+++ TestSchatz/Assets/Scripts/GameConstant.cs
@@ -8,7 +8,13 @@ namespace GameConstant {
         public const float PLAYER_MOVE_SPEED = 0.025f;
 
         //カメラオブジェクトのSizeによって値調整
-        public const float MAIN_CAMERA_WIDTH_SIZE = 4.0f;
+        public const float INIT_CAMERA_POSITION_Z = -10.0f;
 
+        //カメラオブジェクトのSizeによって値調整
+        public const float MAIN_CAMERA_WIDTH_SIZE = 4.8f;
+        public const float MAIN_CAMERA_HEIGHT_SIZE = 3.0f;
+
+        //city1でのy方向のプレイヤー位置初期値
+        public const float CITY1_PLAYER_INIT_POSITION_Y = 1.5f;
   }
 }
