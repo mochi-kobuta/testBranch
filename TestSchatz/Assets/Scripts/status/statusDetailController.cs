179ba089e8435b07eabc76dc8ab09ce012733dc4
yuuki kobayashi <y_kobayashi@serori.co.jp>
2018-11-02 18:57:17 +0900

    テキスト出力 & フォント変更

--- TestSchatz/Assets/Scripts/status/statusDetailController.cs
+++ TestSchatz/Assets/Scripts/status/statusDetailController.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class statusDetailController : MonoBehaviour {
 
-	void Start () {
-		
-	}
-	
-	void Update () {
+    public Text changeTargetText;
+
+    /*
+    public class Parameter
+    {
+        public int IntParam;
+    }
+    public Parameter Param;
+    */
+    void Start()
+    {
+        var textOp = gameObject.GetComponent<textOpen>();
+        changeTargetText.text = textOp.ReadFile();
+        Debug.Log(changeTargetText.text);
+    }
+    
+
+    void Update () {
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene("statusMenu");
---
+++ TestSchatz/Assets/Scripts/textOpen.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO; //System.IO.FileInfo, System.IO.StreamReader, System.IO.StreamWriter
+using System; //Exception
+using System.Text; //Encoding
+using UnityEngine.UI;
+
+public class textOpen : MonoBehaviour {
+
+    private string guitxt = "";
+
+    /*
+    void Update()
+    {
+        // スペースキーを押したらファイル読み込みする
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReadFile();
+        }
+    }
+    // 読み込んだ情報をGUIとして表示
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(5, 5, Screen.width, 50), guitxt);
+    }
+    */
+
+    // 読み込み関数
+    public string ReadFile()
+    {
+        // FileReadTest.txtファイルを読み込む
+        FileInfo fi = new FileInfo(Application.dataPath + "/" + "FileReadTest.txt");
+
+        try
+        {
+            // 一行毎読み込み
+            using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8))
+            {
+                guitxt = sr.ReadToEnd();
+                Debug.Log(guitxt);
+            }
+        }
+        catch (Exception e)
+        {
+            // 改行コード
+            guitxt += SetDefaultText();
+        }
+
+        return guitxt;
+    }
+
+    // 改行コード処理
+    string SetDefaultText()
+    {
+        return "\n";
+    }
+}
