2d41abaea6e48e85c2901c95823c4f36681a5e76
yuuki kobayashi <y_kobayashi@serori.co.jp>
2018-11-14 18:47:53 +0900

    ゲージパーツ追加

---
+++ TestSchatz/Assets/Scripts/Common/TpGageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TpGageController : MonoBehaviour {
+
+    Slider gage;
+    void Start()
+    {
+        // スライダーを取得する
+        gage = GameObject.Find("gage").GetComponent<Slider>();
+    }
+
+    float _hp = 0;
+    void Update()
+    {
+        // HP上昇
+        _hp += 0.01f;
+        if (_hp > 1)
+        {
+            // 最大を超えたら0に戻す
+            _hp = 0;
+        }
+
+        // HPゲージに値を設定
+        gage.value = _hp;
+    }
+}
